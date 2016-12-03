df801adc76f1bb2fdb34f60eb07ba930e14cd289
PieterFabry <Pieterfabry@gmail.com>
2016-12-03 15:34:44 +0000

    Updating to Chris's specs

--- js/basicJS.js
+++ js/basicJS.js
@@ -1,5 +1,44 @@
-$(document).ready(function(){
-  $('button').click(function(){
-    $('#myModal').modal('show');
-  });
+$(function() {
+    // Get the form.
+    var form = $('#ajax-contact');
+
+    // Get the messages div.
+    var formMessages = $('#messages');
+
+    // Set up an event listener for the contact form.
+    $(form).submit(function(event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+        // TODO
+        // Serialize the form data.
+        var formData = $(form).serialize();
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        }).done(function(response) {
+            // Make sure that the formMessages div has the 'success' class.
+            // $(formMessages).removeClass('error');
+            // $(formMessages).addClass('success');
+            //
+            // // Set the message text.
+            // $(formMessages).text(response);
+            $("#myModal").modal('hide');
+            // Clear the form.
+            $('#name').val('');
+            $('#email').val('');
+            $('#message').val('');
+        }).fail(function(data) {
+            // Make sure that the formMessages div has the 'error' class.
+            $(formMessages).removeClass('success');
+            $(formMessages).addClass('error');
+
+            // Set the message text.
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('Oops! An error occured and your message could not be sent.');
+            }
+        });
+    });
 });
